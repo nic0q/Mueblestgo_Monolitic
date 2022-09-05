e84eb70f6d1135ea289fdd9965922c470ddb53f9
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-09-05 19:57:29 -0300

    feat: txt file read and data group

--- src/main/java/com/tingeso/tingeso/services/ReadfileService.java
+++ src/main/java/com/tingeso/tingeso/services/ReadfileService.java
@@ -1,39 +1,55 @@
 package com.tingeso.tingeso.services;
 import java.io.FileInputStream;
 import java.io.InputStream;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class ReadfileService {
-    String entrada = "08:00";
-    String salida = "18:00";
+    String entrada = "08:00"; // Todos los empleados deben llegar a las 8:00
+    String salida = "18:00"; // Todos los empleados deben salir a las 18:00 (Pueden haber horas extras)
+    String minutos_obligatorios = "720"; // Todos los empleados deben trabajar 720 minutos (10 horas)
     public static void main(String args[]) throws Exception {
         InputStream ins = new FileInputStream("src/main/resources/txt/data.txt");
-        
+        ArrayList<String> dias = new ArrayList<String>();    
+        Map<String, ArrayList<String>> ruts_map = new HashMap<String,ArrayList<String>>();
         try (Scanner obj = new Scanner(ins)) {
             while (obj.hasNextLine()){
-                String[] stringarray = obj.nextLine().split(";"); //splitting the line into an array of strings
-                String date = stringarray[0];
-                String entrada = stringarray[1];
-                String salida = stringarray[2];
-                System.out.println(date + " " + entrada + " " + salida);
-            }
-        }   
-    }
-    // I: null
-    // O: String
-    // Read the .txt file and group by day
-    public String[] readfile() throws Exception {
-        InputStream ins = new FileInputStream("src/main/resources/txt/data.txt");
-        try (Scanner obj = new Scanner(ins)) {
-            while (obj.hasNextLine()){
-                String[] stringarray = obj.nextLine().split(";"); //splitting the line into an array of strings
-                String day = stringarray[0];
-                String entrada = stringarray[1];
-                String salida = stringarray[2];
-                System.out.println(day + " " + entrada + " " + salida);
-                return stringarray;
+                String[] registro_asistencia = obj.nextLine().split(";"); //splitting the line into an array of strings
+                String date = registro_asistencia[0];   // posicion de la fecha(dia trabajado)
+                String hora = registro_asistencia[1]; // posicion de la hora
+                String rut = registro_asistencia[2]; // posicion del rut
+                if(!dias.contains(date)){
+                    System.out.print("DIA: "+date + "\n");
+                    dias.add(date);
+                    ruts_map.clear(); // Se limpia el hasmap ya que solo es utilizado para 1 día
+                }
+                if (!ruts_map.containsKey(rut)){
+                    ArrayList<String> temp = new ArrayList<>(); // areglo temporal para almacenar las horas (ENTRADA, SALIDA) de un rut
+                    temp.add(hora); // se añade la hora de ENTRADA
+                    ruts_map.put(rut, temp); // Se añade el rut y la hora de ENTRADA al hashmap
+                }
+                else{
+                    ruts_map.get(rut).add(hora);    // Se añade la hora de SALIDA al hashmap
+                    System.out.println("["+rut+"]"+" ENTRADA: " + ruts_map.get(rut).get(0) + " SALIDA: " + ruts_map.get(rut).get(1) + " TIEMPO: " + getWorkedMinutes(ruts_map.get(rut).get(0), ruts_map.get(rut).get(1)) + "min");
+                }
             }
         }
-        return null;
+    }
+    // I: 2 Strings con formato HH:mm
+    // O: Long con la cantidad de minutos trabajados
+    public static long getWorkedMinutes(String start_t, String end_t) throws ParseException{
+        DateFormat dateFormat = new SimpleDateFormat("hh:mm");
+        Date entrada = dateFormat.parse(start_t);
+        Date salida = dateFormat.parse(end_t);
+        long horas_trabajadas = salida.getTime() - entrada.getTime();
+        return  TimeUnit.MILLISECONDS.toMinutes(horas_trabajadas);
     }
 }
