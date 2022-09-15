bfac6030a527e0e282091b48ddab2f0beb83551a
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-09-15 03:00:52 -0300

    querys

--- Database/monolitic_querys.sql
+++ Database/monolitic_querys.sql
@@ -7,17 +7,15 @@ CREATE TABlE IF NOT EXISTS employee (
 	`name` varchar(20) not null,
 	last_name varchar(25) not null,
     category varchar(5) not null,
-    service_years int,
+    birth_date varchar(20),
+    entry_date varchar(20),
     PRIMARY KEY(rut)
 );
 DROP TABLE employee;
 CREATE TABlE IF NOT EXISTS worked_days (
 	id int auto_increment,
     rut_employee char(10),
-    `date` DATE,
-    `t_entry` char(5),
-    `t_exit` char(5),
-    `worked_hours` int,
+    `date` date,
     `extra_hours` int,
     `late_minutes` int,
     FOREIGN KEY(rut_employee) REFERENCES employee(rut),
@@ -31,31 +29,29 @@ CREATE TABLE IF NOT EXISTS extra_hours(
     FOREIGN KEY(rut_employee) REFERENCES employee(rut),
     PRIMARY KEY(id)
 );
+DROP TABLE extra_hours;
 CREATE TABLE IF NOT EXISTS justificative(
 	id int auto_increment,
-    `description` VARCHAR(200),
-    `date` DATE,
+    `date` varchar(15),
 	rut_employee char(10),
     FOREIGN KEY(rut_employee) REFERENCES employee(rut),
     PRIMARY KEY(id)
 );
 DROP TABLE justificative;
-DROP TABLE worked_days;
-DROP TABLE extra_hours;
 SELECT * FROM employee;
 SELECT * FROM justificative;
 SELECT * FROM extra_hours;
 SELECT * FROM worked_days;
-ALTER TABLE justificative ADD description varchar(500);
-ALTER TABLE employee ADD service_years int;
-ALTER TABLE employee RENAME COLUMN `first_name` TO `name`;
-ALTER TABLE worked_days modify COLUMN `date` varchar(10);
-INSERT INTO employee(rut, `name`,last_name,category,service_years) VALUES("28371904-9","nico","las","A",1);
-INSERT INTO employee(rut, `name`,last_name,category,service_years) VALUES("27391503-6","WEON","SOYYO","A",5);
-INSERT INTO employee(rut, `name`,last_name,category,service_years) VALUES("21142354-k","JUANITO","KIKA","C",10);
-INSERT INTO employee(rut, `name`,last_name,category,service_years) VALUES("12457562-3","JIJAJAJA","las","A",13);
-INSERT INTO employee(rut, `name`,last_name,category,service_years) VALUES("11234123-6","ROBERTO","CARLOS","B",20);
-INSERT INTO employee(rut, `name`,last_name,category,service_years) VALUES("17765876-2","LOCO","RANDOM","B",18);
+
+SELECT * FROM worked_days WHERE `date` > "2022-01-16";
+
+INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("28371904-9","nico","las","A","2000/03/22","2020/03/22");
+INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("27391503-6","WEON","SOYYO","A","2000/03/22","2015/03/22");
+INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("21142354-k","JUANITO","KIKA","C","2000/03/22","2010/03/22");
+INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("12457562-3","JIJAJAJA","las","A","2000/03/22","2005/03/22");
+INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("11234123-6","ROBERTO","CARLOS","B","1999/03/22","2000/03/22");
+INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("17765876-2","LOCO","RANDOM","B","1955/03/22","1999/03/22");
 DELETE FROM employee WHERE id > 0;
+DELETE FROM worked_days WHERE id > 0;
 DELETE FROM extra_hours WHERE id > 0;
 
